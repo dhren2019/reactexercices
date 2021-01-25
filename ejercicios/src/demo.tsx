6e6295e5213558ded0ced5b064205c071f833d99
[Rodrigo] <[roglobar10@gmail.com]>
2021-01-25 20:27:58 +0100

    Commit hooks

--- ejercicios/src/demo.tsx
+++ ejercicios/src/demo.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-export const MyComponent : React.FC = () => {
-const [myName, setMyname] = React.useState('Jhon Doe');
- return (
-   <>
-    Prueba
-    <h4>{myName}</h4>
-    <input value={myName}
-      onChange={(e) => setMyname(e.target.value)}
-     />
-   </>
-      )
-    }
+export const MyChildComponent = () => {
+  return <h4>Hello from child component</h4>
+}
+
+export const MyComponent = () => {
+  const [visible, setVisible] = React.useState(false);
+
+  
+  return <>{visible && <MyChildComponent />}
+  <button onClick={() => setVisible(!visible)}>Click</button>
+  </>
+
+}
+
+//Hooks clase 3.5 min 3
